df6055a0f759e47d9c957f8198b3d34aad5d25d2
Samira Tasnim <samiratasnima@gmail.com>
2025-10-03 21:25:42 +0600

    Add files via upload

---
+++ C. Replacement.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    long long a[n];
+    for(int i=0;i<n;i++){
+		cin>>a[i];
+    }
+    for(int i=0;i<=n;i++){
+if(a[i]>0)
+	a[i]=1;
+else if(a[i]<0)
+	a[i]=2;
+else
+	a[i]=0;
+	}
+	for(int i=0;i<n;i++){
+	cout<<a[i]<<" ";}
+return 0;
+}
